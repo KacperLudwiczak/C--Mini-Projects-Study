f3c865d26bc2919ad2060d57b144472e4164e454
Kacper Ludwiczak <casper.ludwiczak@gmail.com>
2024-10-20 19:25:37 +0200

    Add files via upload

---
+++ Age Checking -KL/Age Checking -KL/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("How old are you?");
+int  age = int.Parse(Console.ReadLine());
+
+
+if (age >= 18)
+{
+    Console.WriteLine("You can go to party.");
+    
+} else if  (age >= 13)
+{
+    Console.WriteLine("Are you with your parents? Yes or No?");
+    string isWithParentsString = Console.ReadLine();
+    if (isWithParentsString == "Yes" || isWithParentsString == "yes")
+    {
+        Console.WriteLine("Go to party with your parents.");
+    } else
+    {
+        Console.WriteLine("You can't go to party.");
+    }
+    
+} else
+{
+    Console.WriteLine("You can't go to party.");
+}
+
+Console.ReadLine();
