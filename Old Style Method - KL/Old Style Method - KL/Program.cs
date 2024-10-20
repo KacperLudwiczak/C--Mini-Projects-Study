c85c5c95334421ed6fb1e72964f1dcb8f5dd6e17
Kacper Ludwiczak <casper.ludwiczak@gmail.com>
2024-10-20 19:33:44 +0200

    Add files via upload

---
+++ Old Style Method - KL/Old Style Method - KL/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Old_Style_Method___KL
+{
+    internal class Program
+    {
+        // Field or instance variable
+        static int myAddResult;
+        static void Main(string[] args)
+        {
+            myAddResult = AddTwoValuesSecond(5, 8);
+            Console.WriteLine($"Result of second adding example is {myAddResult}");
+
+            Console.ReadKey();
+        }
+
+        static int AddTwoValuesSecond(int value1, int value2)
+        {
+            int result = value1 + value2;
+            return result;
+        }
+    }
+}
