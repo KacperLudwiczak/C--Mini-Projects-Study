08feacdd135566cafe616dede93f39ea55fab5fb
Kacper Ludwiczak <casper.ludwiczak@gmail.com>
2025-01-14 11:04:32 +0100

    Add First-Code

---
+++ First-Code/First-Code/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Hello, My name is Kacper!");
+
+// declare a (string) variable
+string myFriendsName;
+// assign a value to the myFriendsName
+myFriendsName = "Tom";
+// use/access the variable
+Console.WriteLine(myFriendsName);
+// overwriting the variable value
+myFriendsName = "John";
+// use/access the variable
+Console.WriteLine(myFriendsName);
+
+// It prints out whatever is in side of ()
+Console.Write("Please enter: ");
+
+// It takes user input and stores it
+// Variable with the name "userInput" and the data type "string"
+string userInput = Console.ReadLine();
+
+// We are adding two strings together and writing them in to the console
+Console.WriteLine($"You entered: {userInput}");
+
+Console.ReadKey();
